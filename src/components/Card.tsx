76abc2cca208d9fae13946017236098493100f70
Víctor Ariel Pagano <Victor.Ariel.Pagano@ibm.com>
2021-05-09 22:11:47 -0300

    Image lazy load

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ImageFadeIn from "react-image-fade-in";
+
 import useModal from '../hooks/useModal';
+
+// Services
 import { truncate } from '../services/stringService';
+
+// Components
 import Modal from './Modal';
 
 export interface ICardProps {
@@ -9,7 +15,7 @@ export interface ICardProps {
   link: string;
   imageSrc: string;
   imageAlt?: string;
-  loading: boolean
+  loading: boolean;
 }
 
 const Card: React.FC<ICardProps> = ({
@@ -18,38 +24,57 @@ const Card: React.FC<ICardProps> = ({
   link,
   imageSrc,
   imageAlt,
-  loading
+  loading,
 }) => {
   const { isShowing, toggle } = useModal();
-  const [endLoading, setEndLoading] = useState();
 
- /*  useEffect(() => {
-    if (loading) {
-      set
-    }
-    return () => {
-      cleanup
-    }
-  }, [input])
- */
   return (
     <>
       <div className="card w-100 box-shadow img-hover">
-      {imageSrc && <div onClick={toggle} className="overflow-hidden relative h-200 bg-gradient-placeholder">{<img src={imageSrc} className="card-img-top object-cover w-100 h-100" alt={imageAlt} /> }</div>}
+        {imageSrc && (
+          <div
+            onClick={toggle}
+            className="overflow-hidden relative h-200 bg-gradient-placeholder"
+          >
+            {
+            
+           !loading && <ImageFadeIn width={640} height={480} src={imageSrc} className="card-img-top object-cover w-100 h-100"
+           alt={imageAlt}/>
+
+            }
+          </div>
+        )}
         <div className="card-body d-flex flex-column flex-fill justify-content-between">
           <div className="mb-3">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{truncate(body, 20)}</p>
           </div>
-          <a href={link} className="align-self-end" target="_blank" rel="noreferrer">Read more</a>
+          <a
+            href={link}
+            className="align-self-end"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Read more
+          </a>
         </div>
       </div>
-        {<Modal
+      {
+        <Modal
           isShowing={isShowing}
           hide={toggle}
           headerText={title}
-          modalContent={<div className="min-h-200 bg-gradient-placeholder"><img src={imageSrc} alt={imageAlt} className="img-fluid rounded w-100"/></div>}
-        />}
+          modalContent={
+            <div className="min-h-200 bg-gradient-placeholder">
+              <img
+                src={imageSrc}
+                alt={imageAlt}
+                className="img-fluid rounded w-100"
+              />
+            </div>
+          }
+        />
+      }
     </>
   );
 };
